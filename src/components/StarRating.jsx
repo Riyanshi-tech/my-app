9fcd9b14a02bc2722e08f9a0e73fd87b97c6d59d
Riyanshi1 <riyanshichaudhary449@gmail.com>
2025-09-21 20:49:52 +0530

    starrating removed

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,18 +1,18 @@
-import React from "react";
+// import React from "react";
 
-const StarRating = ({ rating, maxStars = 5 }) => {
-  return (
-    <div style={{ display: "flex", gap: "4px" }}>
-      {Array.from({ length: maxStars }, (_, i) => (
-        <img
-          key={i}
-          src={i < rating ? "/star-filled.png" : "/star-empty.png"} 
-          alt="star"
-          style={{ width: 24, height: 24 }}
-        />
-      ))}
-    </div>
-  );
-};
+// const StarRating = ({ rating, maxStars = 5 }) => {
+//   return (
+//     <div style={{ display: "flex", gap: "4px" }}>
+//       {Array.from({ length: maxStars }, (_, i) => (
+//         <img
+//           key={i}
+//           src={i < rating ? "/star-filled.png" : "/star-empty.png"} 
+//           alt="star"
+//           style={{ width: 24, height: 24 }}
+//         />
+//       ))}
+//     </div>
+//   );
+// };
 
-export default StarRating;
+// export default StarRating;
